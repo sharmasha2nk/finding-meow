62a153c6814be93fd0f64d039bf2a1273719b388
Shashank Sharma <shashank@active.ai>
2020-05-24 17:30:48 +0800

    Add MoesifFilter

---
+++ src/main/java/com/findcat/esdemo/MyConfig.java
@@ -0,0 +1,19 @@
+package com.findcat.esdemo;
+
+import javax.servlet.Filter;
+
+import com.moesif.servlet.MoesifFilter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Filter moesifFilter() {
+        String moesifApplicationId = System.getenv().get("MOESIF_APPLICATION_ID");
+        return new MoesifFilter(moesifApplicationId);
+    }
+}
